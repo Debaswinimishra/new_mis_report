965994de9fdb81584e16258510747636fe09f2d8
Debaswinimishra <debaswinimishra@gmail.com>
2023-09-14 12:57:46 +0530

    changes in css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -62,12 +62,9 @@
 
 import React, { useState } from "react";
 import PeopleIcon from "@mui/icons-material/People";
-import SchoolIcon from "@mui/icons-material/School";
 import QuizIcon from "@mui/icons-material/Quiz";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import { Link, useNavigate } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -76,27 +73,10 @@ import MenuItem from "@mui/material/MenuItem";
 import Avatar from "@mui/material/Avatar";
 import Logout from "@mui/icons-material/Logout";
 import Swal from "sweetalert2";
+import SchoolIcon from "@mui/icons-material/School";
 
 function Home() {
   const navigate = useNavigate();
-
-  // const listItem = [
-  //   {
-  //     text: "Fellows",
-  //     link: "/home/fellows",
-  //     icon: <PeopleIcon fontSize="large" style={{ color: "#2196F3" }} />,
-  //   },
-  //   {
-  //     text: "School",
-  //     link: "/home/school",
-  //     icon: <SchoolIcon fontSize="large" style={{ color: "#4CAF50" }} />,
-  //   },
-  //   {
-  //     text: "Anganwadi",
-  //     link: "/home/anganwadi",
-  //     icon: <QuizIcon fontSize="large" style={{ color: "#FF5722" }} />,
-  //   },
-  // ];
 
   const [anchorEl, setAnchorEl] = useState(null);
 
@@ -125,31 +105,40 @@ function Home() {
 
   return (
     <>
-      <div className="background-image">
+      <div>
         <Box
-          // backgroundColor="red"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height="calc(100vh - 56px)" // Adjusted to accommodate the AppBar
-          marginTop="56px" // Add margin to avoid content being covered by AppBar
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "calc(100vh - 56px)",
+            marginTop: "56px",
+          }}
         >
           <Card
-            // style={{ ...styles.CenteredDiv, backgroundColor: "#3498db" }}
             sx={{
               width: "250px",
               height: "200px",
               textAlign: "center",
               margin: "10px",
-              paddingTop: "50px",
               borderRadius: "10px",
-              alignContent: "space-between",
+              cursor: "pointer",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              background: "linear-gradient(to bottom, #ff6b6b, #f0a881)",
+              justifyContent: "center", // Center content vertically
             }}
             onClick={() => {
               navigate("/fellow");
             }}
           >
-            Fellow
+            <PeopleIcon
+              sx={{
+                fontSize: 48,
+              }}
+            />
+            <span style={{ fontSize: "20px" }}>Fellow</span>
           </Card>
           <Card
             sx={{
@@ -157,53 +146,69 @@ function Home() {
               height: "200px",
               textAlign: "center",
               margin: "10px",
-              paddingTop: "50px",
               borderRadius: "10px",
-              alignContent: "space-between",
+              cursor: "pointer",
+              display: "flex",
+              background: "linear-gradient(to bottom, #ff6b6b, #f0a881)",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center", // Center content vertically
             }}
-            // style={{ ...styles.CenteredDiv, backgroundColor: "#e74c3c" }}
-            onClick={() => {
-              navigate("/anganwadi");
-            }}
-          >
-            Anganwadi
-          </Card>
-          <Card
-            sx={{
-              width: "250px",
-              height: "200px",
-              textAlign: "center",
-              margin: "10px",
-              paddingTop: "50px",
-              borderRadius: "10px",
-              alignContent: "space-between",
-            }}
-            // style={{ ...styles.CenteredDiv, backgroundColor: "#27ae60" }}
             onClick={() => {
               navigate("/school");
             }}
           >
-            School
+            <SchoolIcon
+              sx={{
+                fontSize: 48,
+              }}
+            />
+            <span style={{ fontSize: "20px" }}>School</span>
+          </Card>
+          <Card
+            sx={{
+              width: "250px",
+              height: "200px",
+              textAlign: "center",
+              margin: "10px",
+              borderRadius: "10px",
+              cursor: "pointer",
+              display: "flex",
+              background: "linear-gradient(to bottom, #ff6b6b, #f0a881)",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center", // Center content vertically
+            }}
+            onClick={() => {
+              navigate("/anganwadi");
+            }}
+          >
+            <QuizIcon
+              sx={{
+                fontSize: 48,
+              }}
+            />
+            <span style={{ fontSize: "20px" }}>Anganwadi</span>
           </Card>
         </Box>
 
         <IconButton
-          color="inherit"
-          aria-label="menu"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
+          // color="inherit"
+          // aria-label="menu"
+          // aria-controls="menu-appbar"
+          // aria-haspopup="true"
           onClick={handleMenuClick}
           sx={{
             position: "fixed",
             top: "10px",
             right: "10px",
             backgroundColor: "#FFF",
-            borderRadius: "50%",
+            // borderRadius: "10%",
           }}
         >
           <Avatar
             alt="Logo"
-            src="https://thinkzone.in/wp-content/uploads/2022/06/Instagram-1-1-1-1-2.png" // Replace with the actual path to your logo image
+            src="https://thinkzone.in/wp-content/uploads/2022/06/Instagram-1-1-1-1-2.png"
           />
         </IconButton>
         <Popover

0b8b5f6aa75b1e132f33f640252ec55d44c42501
MonalisaMoharana2000 <monalisamoharana99@gmail.com>
2023-09-25 17:03:38 +0530

    check

--- src/pages/Fellow/CommunityEducator/CommunityEducator.js
+++ src/pages/Fellow/CommunityEducator/CommunityEducator.js
@@ -91,9 +91,9 @@ const ComunityEducator = () => {
         console.log("err--->", err.response.status);
       }
     } else if (selectedTabIndex === 1) {
-      setManagerName([]);
-      setSelectedYear([]);
-      setPasscode([]);
+      setManagerName("");
+      setSelectedYear("");
+      setPasscode("");
       setTab1FilterData({});
       try {
         const response1 = await getAllCommunityEducatiorFilter();
@@ -146,9 +146,13 @@ const ComunityEducator = () => {
   });
   const handleYearChange = (selectedYear) => {
     setSelectedYear(selectedYear);
+    setManagerName("");
+    setPasscode("");
   };
   const handleYearChangeTab2 = (selectedYearTab2) => {
     setSelectedYearTab2(selectedYearTab2);
+    setManagerNameTab2("");
+    setPasscodeTab2("");
   };
   const handleManagerChange = (event) => {
     setManagerName(event.target.value);

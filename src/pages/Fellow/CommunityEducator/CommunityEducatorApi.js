2b71e8a493e128fd5e1e687ed589191233025f30
dhaneswar Setha <dhaneswarsetha.123@gmail.com>
2023-10-13 16:59:43 +0530

    comunity api

--- src/pages/Fellow/CommunityEducator/CommunityEducatorApi.js
+++ src/pages/Fellow/CommunityEducator/CommunityEducatorApi.js
@@ -4,14 +4,13 @@ export const getAllCommunityEducatiorFilter = async () =>
   await Api.get(getManagerIdsWidPasscode);
 
 export const getAllDistricts = async () =>
-  await Api.get(getdistrictsofstate/20);
+  await Api.get(getdistrictsofstate / 20);
 
 export const getDistrictsWiseBlocks = async (blockId) =>
-  await Api.get(getblocksofdistricts/20/${blockId});
+  await Api.get(`getblocksofdistricts/20/${blockId}`);
 
 export const getCommunityEducator1 = async (body) =>
   await Api.get(getDashboardCounts, body);
 
 export const getCommunityEducator2 = async (body) =>
   await Api.get(getDashboardCounts, body);
-

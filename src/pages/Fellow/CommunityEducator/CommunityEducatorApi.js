e707d9508d2ebcaf292acbb3e10b9a5501ba104d
dhaneswar Setha <dhaneswarsetha.123@gmail.com>
2023-10-13 17:00:40 +0530

    api

--- src/pages/Fellow/CommunityEducator/CommunityEducatorApi.js
+++ src/pages/Fellow/CommunityEducator/CommunityEducatorApi.js
@@ -1,16 +1,22 @@
 import Api from "../../../Environment/Api";
 
 export const getAllCommunityEducatiorFilter = async () =>
-  await Api.get(getManagerIdsWidPasscode);
+  await Api.get(`getManagerIdsWidPasscode`);
 
 export const getAllDistricts = async () =>
-  await Api.get(getdistrictsofstate / 20);
+  await Api.get(`getdistrictsofstate/20`);
 
 export const getDistrictsWiseBlocks = async (blockId) =>
   await Api.get(`getblocksofdistricts/20/${blockId}`);
 
 export const getCommunityEducator1 = async (body) =>
-  await Api.get(getDashboardCounts, body);
+  await Api.get(`getDashboardCounts`, body);
 
 export const getCommunityEducator2 = async (body) =>
-  await Api.get(getDashboardCounts, body);
+  await Api.get(`getDashboardCounts`, body);
+
+// useEffect(() => {
+//   Api.get(`getManagerIdsWidPasscode`).then((response) => {
+//     setManagerArr(response.data.resData);
+//   });
+// }, []);

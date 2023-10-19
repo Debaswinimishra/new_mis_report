195885da1a0625fa48055885ada8039679b81d93
debaswinimishra <debaswinimishra@gmail.com>
2023-10-19 13:00:37 +0530

    cmq mark percentage add

--- src/pages/Fellow/CommonMonthlyQuiz/CommonMonthlyQuiz.js
+++ src/pages/Fellow/CommonMonthlyQuiz/CommonMonthlyQuiz.js
@@ -287,6 +287,7 @@ const CommonMonthlyQuiz = () => {
     "Topic name",
     "Secured Mark",
     "Total Mark",
+    "Percentage"
   ];
 
   const getCellValue = (row, column, index) => {
@@ -310,7 +311,7 @@ const CommonMonthlyQuiz = () => {
       case "Secured Mark":
         return row.securedMarks;
         case "Percentage":
-          return row.totalMarks;
+          return row.percentage;
       default:
         return "";
     }

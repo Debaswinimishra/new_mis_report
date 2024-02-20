e0bac33c9d2cbec80c475f22f19a026045db30f7
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-02-20 13:11:04 +0530

    Added a new api folder for prakashak

---
+++ src/pages/Prakashak/Environment/Api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+// export const baseURL = "https://tatvagyan.co.in/prakashak/";
+export const baseURL = "https://tatvagyan.in/prakashak";
+
+export const Version = {
+  version: "1.2.2",
+};
+export default axios.create({
+  baseURL,
+});

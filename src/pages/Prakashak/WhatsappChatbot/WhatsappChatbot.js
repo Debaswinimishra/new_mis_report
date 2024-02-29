03601eed809de5a803620212bb81ed43330dae6c
dhaneswar Setha <dhaneswarsetha.123@gmail.com>
2024-02-29 11:02:45 +0530

    whatsapp

--- src/pages/Prakashak/WhatsappChatbot/WhatsappChatbot.js
+++ src/pages/Prakashak/WhatsappChatbot/WhatsappChatbot.js
@@ -1,56 +1,513 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import Card from "../../../ReusableComponents/Card";
+import PeopleIcon from "@mui/icons-material/People";
+import Box from "@mui/material/Box";
+import Api from "../Environment/Api";
+import Nodata from "../../../Assets/Nodata.gif";
 
 const WhatsappChatbot = () => {
-  const handleFilter = () => {
-    // console.log("school--->", e.target.value);
-    alert("clicked");
+  //?---------------Month array---------------------------
+  const monthArr = [
+    { value: "0", label: "Select Month" },
+    { value: "1", label: "January" },
+    { value: "2", label: "February" },
+    { value: "3", label: "March" },
+    { value: "4", label: "April" },
+    { value: "5", label: "May" },
+    { value: "6", label: "June" },
+    { value: "7", label: "July" },
+    { value: "8", label: "August" },
+    { value: "9", label: "September" },
+    { value: "10", label: "October" },
+    { value: "11", label: "November" },
+    { value: "12", label: "December" },
+  ];
+
+  //?-----------------Week array--------------------
+  const weekArr = [
+    { value: 1, label: "1" },
+    { value: 2, label: "2" },
+    { value: 3, label: "3" },
+    { value: 4, label: "4" },
+  ];
+
+  //?----------------Class Array-----------------------
+  const classArr = [
+    { value: 1, label: "1" },
+    { value: 2, label: "2" },
+    { value: 3, label: "3" },
+    { value: 4, label: "4" },
+  ];
+
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: 5 }, (_, index) => currentYear - index);
+
+  //&-------------Filter states---------------
+  const [selectedClass, setSelectedClass] = useState("");
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = useState(monthArr[0].value);
+  const [selectedWeek, setSelectedWeek] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleYearChange = (e) => {
+    setSelectedYear(e.target.value);
+  };
+
+  const handleMonthChange = (e) => {
+    setSelectedMonth(e.target.value);
+  };
+
+  const handleWeekChange = (e) => {
+    setSelectedWeek(e.target.value);
+  };
+
+  // const handleClassChange = (e) => {
+  //   setSelectedClass(e.target.value);
+  // };
+
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    setLoading(true);
+    const body = {
+      year: selectedYear,
+    };
+
+    console.log("check---------->", selectedYear, selectedMonth, selectedWeek);
+
+    if (selectedYear) {
+      Api.post(`getChatbotReport`, body)
+        .then((response) => {
+          console.log("set=================>", response.data);
+          setData(response.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log("err=================>", err);
+          setLoading(false);
+        });
+    } else {
+      Api.post(`getChatbotReport`)
+        .then((response) => {
+          console.log("set=================>", response.data);
+          setData(response.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log("err=================>", err);
+          setLoading(false);
+        });
+    }
+  };
+
+  const filterButtonClick = () => {
+    setLoading(true);
+    fetchData();
   };
   return (
-    <div style={{ marginTop: "5%" }}>
-      {/* <div style={{ marginRight: "15%", marginTop: "5%" }}>
-        <button
-          style={{
-            border: "2px solid rgb(65, 105, 225)",
-            padding: "1%",
-            cursor: "pointer",
-            borderRadius: "5px",
-            backgroundColor: "rgb(65, 105, 225)",
-            width: "10rem",
-            height: "3rem",
-            marginTop: "-3.9%",
-            marginRight: "-9%",
-          }}
-          // value={item.id}
-          onClick={() => handleFilter()}
-        >
-          <h3 style={{ color: "white", marginTop: "-1.9%" }}>Filter</h3>
-        </button>
-      </div> */}
-
-      {/* <div
+    <div>
+      <div
         style={{
-          // display: "flex",
+          display: "flex",
+          // justifyContent: "space-around",
+          // width: "25%",
+          marginTop: "4%",
+          marginLeft: "48%",
+          // width: "30%",
           flexWrap: "wrap",
-          justifyContent: "space-around",
-          width: "100%",
         }}
       >
+        <FormControl sx={{ m: 1 }} size="small" style={{ width: "120px" }}>
+          <InputLabel id="usertype-label">Year</InputLabel>
+          <Select
+            labelId="usertype-label"
+            id="usertype-select"
+            value={selectedYear}
+            onChange={handleYearChange}
+            label="Year"
+          >
+            {years.map((item, index) => (
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1 }} size="small" style={{ width: "120px" }}>
+          <InputLabel id="usertype-label">Month</InputLabel>
+          <Select
+            labelId="usertype-label"
+            id="usertype-select"
+            value={selectedMonth}
+            onChange={handleMonthChange}
+            label="Month"
+          >
+            {monthArr.map((item, index) => (
+              <MenuItem key={index} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 1 }} size="small" style={{ width: "120px" }}>
+          <InputLabel id="usertype-label">Week</InputLabel>
+          <Select
+            labelId="usertype-label"
+            id="usertype-select"
+            value={selectedWeek}
+            onChange={handleWeekChange}
+            label="Month"
+          >
+            {weekArr.map((item, index) => (
+              <MenuItem key={index} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <Button
+          variant="contained"
+          sx={{
+            height: "40px",
+            width: "120px",
+            marginTop: "1.2%",
+          }}
+          onClick={filterButtonClick}
+        >
+          Filter
+        </Button>
+      </div>
+
+      {/* ---------------------------- content --------------------- */}
+      {loading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}
+        >
+          <Box>
+            <CircularProgress />
+          </Box>
+        </div>
+      ) : !loading && Object.keys(data).length > 0 ? (
         <div
           style={{
-            backgroundColor: "#f3f2ff",
-            width: "250px",
-            height: "120px", // Increased the height to accommodate all child divs
-            borderRadius: "10px",
-            border: "1px solid #000", // Added a border for better visualization
-            margin: "5px", // Added margin for better spacing
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            width: "96%",
+            marginLeft: "3%",
+            marginBottom: "2%",
+            marginTop: "2%",
           }}
         >
-          <h1 style={{ color: "rgb(65, 105, 225)" }}>255</h1>
-          <h3>Total No. of Students </h3>
-        </div>
-      </div> */}
+          <div
+            style={{
+              marginTop: "2%",
+              boxShadow: "2px 1px 5px grey",
+              padding: "3%",
+              width: "97%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignContent: "center",
+                justifyContent: "center",
+                width: "97%",
+                gap: "2%",
+              }}
+            >
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "#CD5C5C",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Total No. of Users
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "#CD5C5C",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.chatbot_users}</h1>
+                </div>
+              </div>
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "rgb(214 148 16)",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Total No. of New Users
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "rgb(214 148 16)",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.chatbot_new_users}</h1>
+                </div>
+              </div>
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "#6A5ACD",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Total No. of Active Users
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "#6A5ACD",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.chatbot_active_users}</h1>
+                </div>
+              </div>
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "#2E8B57",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Total No. of Parents completed at least one activity
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "#2E8B57",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.chatbot_users_completed_one_act}</h1>
+                </div>
+              </div>
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "rgb(153 58 134)",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Total No. of parents completed the full thread
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "rgb(153 58 134)",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.chatbot_users_completed_full_thread}</h1>
+                </div>
+              </div>
 
-      <h1>This module is under development</h1>
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "#6A5ACD",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Total No. of Minutes Spent
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "#6A5ACD",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.total_chatbot_mins}</h1>
+                </div>
+              </div>
+              <div
+                style={{
+                  width: "255px",
+                  height: "180px",
+                  marginTop: "1.5%",
+                  backgroundColor: "white",
+                  // // paddingTop: "2%",
+                  // fontFamily: "Arial, sans-serif", // Default font family
+                  borderRadius: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  boxShadow: "1px 1px 4px 3px lightGrey",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50%",
+                    color: "rgb(153 58 134)",
+                    paddingTop: "20px",
+                    fontSize: "1.2rem",
+                    fontFamily: "Congenial SemiBold",
+                    fontWeight: "600",
+                  }}
+                >
+                  Average Minutes Spent
+                </div>
+                <div
+                  style={{
+                    height: "50%",
+                    backgroundColor: "rgb(153 58 134)",
+                    borderEndStartRadius: "10px",
+                    borderEndEndRadius: "10px",
+                    color: "white",
+                  }}
+                >
+                  <h1>{data.chatbot_avg_mins}</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : !loading && Object.keys(data).length === 0 ? (
+        <img src={Nodata} />
+      ) : null}
     </div>
   );
 };

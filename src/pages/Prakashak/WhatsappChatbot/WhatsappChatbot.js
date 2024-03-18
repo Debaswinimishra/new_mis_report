346b8aa48cb849029cb73f18b7a8ce5fef8a556e
dhaneswar Setha <dhaneswarsetha.123@gmail.com>
2024-03-18 12:16:43 +0530

    add week 5 in the dropdown

--- src/pages/Prakashak/WhatsappChatbot/WhatsappChatbot.js
+++ src/pages/Prakashak/WhatsappChatbot/WhatsappChatbot.js
@@ -46,6 +46,7 @@ const WhatsappChatbot = () => {
     { value: 2, label: "2" },
     { value: 3, label: "3" },
     { value: 4, label: "4" },
+    { value: 5, label: "5" },
   ];
 
   //?----------------Class Array-----------------------

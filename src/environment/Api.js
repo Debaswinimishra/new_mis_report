73df243a7b135b12f7056689735d3c66fac9bb07
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2024-06-24 15:59:24 +0530

    version changes

--- src/environment/Api.js
+++ src/environment/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = "https://thinkzone.co/thinkzone"; //Prod
 
 export const Version = {
-  version: "1.2.5",
+  version: "1.2.7",
 };
 export default axios.create({
   baseURL,

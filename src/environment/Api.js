f27c0d6fae33a737a5528a482ab1e1a7e53c5238
Swastik Giri <swastikrancodes@gmail.com>
2024-06-26 01:05:36 +0530

    changes version change

--- src/environment/Api.js
+++ src/environment/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = "https://thinkzone.co/thinkzone"; //Prod
 
 export const Version = {
-  version: "1.2.8",
+  version: "1.2.9",
 };
 export default axios.create({
   baseURL,

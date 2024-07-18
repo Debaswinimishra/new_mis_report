9ac64213fb7d62a94b4ff95b61a3d80cddfc0614
dhaneswar Setha <dhaneswarsetha.123@gmail.com>
2024-07-18 13:37:23 +0530

    dashboard

--- src/pages/Prakashak/Dashboard/Dashboard.js
+++ src/pages/Prakashak/Dashboard/Dashboard.js
@@ -269,6 +269,27 @@ const Dashboard = () => {
 
   const getModalTitle = (type) => {
     switch (type) {
+      case "district":
+        return "Number of District";
+      case "block":
+        return "Number of blocks";
+
+      case "clusters":
+        return "Number of Clusters";
+      case "schools":
+        return "Number of Schools";
+      case "newSchools":
+        return "Total number of new schools";
+      case "newStudents":
+        return "Total number of Smartphone users";
+
+      case "smartphoneUsers":
+        return "Total number of Smartphone users";
+      case "girls":
+        return "Total Number of girls";
+      case "boys":
+        return "Total Number of Boys";
+
       case "students":
         return "Number of Students";
       case "calls":
@@ -330,7 +351,7 @@ const Dashboard = () => {
             block: block.block,
           }));
           setModalLoader(false);
-          setTableHeaders(["Sl. No", "District", "Block"]);
+          setTableHeaders(["Block"]);
         }
       } else if (type === "clusters") {
         response = await PrakashakAPI.post("getAllClusters", body);

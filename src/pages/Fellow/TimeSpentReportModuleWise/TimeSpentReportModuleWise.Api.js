5ed1d0d97dce6024076e1191e950b26b0e3218b1
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2024-07-26 11:23:39 +0530

    changes in api

--- src/pages/Fellow/TimeSpentReportModuleWise/TimeSpentReportModuleWise.Api.js
+++ src/pages/Fellow/TimeSpentReportModuleWise/TimeSpentReportModuleWise.Api.js
@@ -1,8 +1,8 @@
 import Api from "../../../Environment/Api";
 
-export const getManagerWidPasscodes = async (year) => {
-  return await Api.get(`getManagerIdsWidPasscode/${year}`);
-};
+// export const getManagerWidPasscodes = async (year) => {
+//   return await Api.get(`getManagerIdsWidPasscode/${year}`);
+// };
 
 export const getAllTimespentData = async (body) => {
   return await Api.post(`getDetailedTimeSpentReport`, body);

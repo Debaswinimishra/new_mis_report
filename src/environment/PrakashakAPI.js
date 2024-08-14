9bcc82d124a1c6e5eca9f960b5c9e367ea9284d0
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2024-08-14 13:10:17 +0530

    version changes

--- src/environment/PrakashakAPI.js
+++ src/environment/PrakashakAPI.js
@@ -3,7 +3,7 @@ export const baseURL = "https://tatvagyan.co.in/prakashak/";
 // export const baseURL = "https://tatvagyan.in/prakashak";
 
 export const Version = {
-  version: "1.3.5",
+  version: "1.3.6",
 };
 
 export const networkStatus =

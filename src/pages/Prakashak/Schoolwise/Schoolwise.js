dafa7bc255d00fda5558fb5820792edcfcfd93ad
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2024-08-21 15:56:36 +0530

    new commit

--- src/pages/Prakashak/Schoolwise/Schoolwise.js
+++ src/pages/Prakashak/Schoolwise/Schoolwise.js
@@ -541,6 +541,24 @@ const Schoolwise = () => {
               width: "97%",
             }}
           >
+            <div style={{ marginTop: "-2%" }}>
+              <h1
+                style={{
+                  marginTop: "-2%",
+                  color: "#333", // Dark grey color for the text
+                  fontFamily: "Congenial SemiBold", // Font family for a clean look
+                  fontWeight: "700", // Bolder font weight for emphasis
+                  fontSize: "1.8rem", // Larger font size for prominence
+                  textAlign: "center", // Center-align the text
+                  padding: "10px 0", // Add some padding for spacing
+                  borderBottom: "2px solid #000000", // Add a bottom border for separation
+                  letterSpacing: "0.5px", // Slight letter spacing for readability
+                  textTransform: "capitalize", // Capitalize each word
+                }}
+              >
+                School-wise Details
+              </h1>
+            </div>
             <div
               style={{
                 display: "flex",

901427acb8ba2bf241ebb18254106e455f783fe5
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2024-12-13 10:54:21 +0530

    changes

--- src/pages/Prakashak/Retention/Retention.js
+++ src/pages/Prakashak/Retention/Retention.js
@@ -54,7 +54,7 @@ export default function Retention() {
   ];
 
   const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: 1 }, (_, index) => currentYear - index);
+  const years = Array.from({ length: 1 }, (_, index) => currentYear - index);                                                                        
   const [selectedYear, setSelectedYear] = useState(currentYear);
   const handleYearChange = (e) => {
     setSelectedYear(parseInt(e.target.value));
